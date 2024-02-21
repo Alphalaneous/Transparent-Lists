d52df07970c55ebfc4c2dd0a3c77af4d002dcded
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2024-02-21 17:26:31 -0500

    Fix a bug

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,10 @@ class $modify(GJListLayer) {
 	static GJListLayer* create(BoomListView* p0, char const* p1, cocos2d::ccColor4B p2, float p3, float p4, int p5) {
 		auto ret = GJListLayer::create(p0, p1, p2, p3, p4, p5);
 		
-
-		ret->setColor({0,0,0});
-		ret->setOpacity(50.0f);
+		if(ret->getColor() == ccColor3B{191,114,62}){
+			ret->setColor({0,0,0});
+			ret->setOpacity(50.0f);
+		}
 
 		return ret;
 	}
